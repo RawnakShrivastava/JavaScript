42fecf59f61817ec5e7891ae4ffb8329cfa11c52
Rawnak04 <30366792+Rawnak04@users.noreply.github.com>
2020-11-02 20:13:49 +0530

    Removing Security_Project

--- SecurityPage/src/components/filemanager.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class FileManager extends React.Component {
-  render() {
-    return <div>Welcome to File Manager</div>;
-  }
-}
-
-export default FileManager;
--- SecurityPage/src/util/util.js
+++
@@ -1,1 +0,0 @@
-export const domain= process.env.DOMAIN;
